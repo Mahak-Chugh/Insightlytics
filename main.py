963125afbbd4a8b62c3df0b1fe919e413d424d83
Mahak Chugh <159686135+Mahak-Chugh@users.noreply.github.com>
2025-02-21 23:33:34 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-import plotly.express as px
+#import plotly.express as px
 
 # Set page configuration
 st.set_page_config(page_title="Data Analysis App", layout="wide", page_icon="📊")
